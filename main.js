305424215c6840928048fa3781cabe1dfd5b3f55
Alilouspamzz <159388851+Alilouspamzz@users.noreply.github.com>
2024-02-08 10:51:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let title = document.querySelector(".title");
 let turn = "x";
 let square = [];
-
+let count = 0;
 function game(id) {
   let element = document.getElementById(id);
   if (turn === "x" && element.innerHTML == "") {
@@ -23,31 +23,39 @@ function winner() {
   // horizontal
   if (square[1] == square[2] && square[2] == square[3] && square[1] != "") {
     newGame(1, 2, 3);
+    count++;
   }
   if (square[4] == square[5] && square[5] == square[6] && square[4] != "") {
     newGame(4, 5, 6);
+      count++;
   }
   if (square[7] == square[8] && square[8] == square[9] && square[7] != "") {
     newGame(7, 8, 9);
+        count++;
   }
 
   // vertical
   if (square[1] == square[4] && square[4] == square[7] && square[1] != "") {
     newGame(1, 4, 7);
+        count++;
   }
   if (square[2] == square[5] && square[5] == square[8] && square[2] != "") {
     newGame(2, 5, 8);
+        count++;
   }
   if (square[3] == square[6] && square[6] == square[9] && square[3] != "") {
     newGame(3, 6, 9);
+    count++;
   }
 
   //cros
   if (square[1] == square[5] && square[5] == square[9] && square[1] != "") {
     newGame(1, 5, 9);
+        count++;
   }
   if (square[3] == square[5] && square[5] == square[7] && square[3] != "") {
     newGame(3, 5, 7);
+        count++;
   } 
   if (
     square[1] != "" &&
@@ -58,7 +66,8 @@ function winner() {
     square[6] != "" &&
     square[7] != "" &&
     square[8] != "" &&
-    square[9] != ""
+    square[9] != "" &&
+    count != 0
   ) {
     title.innerHTML = "Drow";
     setInterval(function () {
